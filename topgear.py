5fb233875e109cad85bd56f3c7d13550ef5a38b7
Krishna Srinivas <32317345+Krish6987@users.noreply.github.com>
2019-12-04 14:41:50 +0530

    Update topgear.py

--- topgear.py
+++ topgear.py
@@ -132,3 +132,158 @@ for i in range(len(inp)-1):
 
 
 print(inp)
+
+
+
+#####5
+inp = input().split(" ")
+
+for i in range(len(inp)):
+
+    inp[i] = int(inp[i])
+
+
+result = [inp[0]]
+
+
+for i in inp:
+
+    if i != result[-1]:
+
+        result.append(i)
+
+print(result)
+
+#####6
+def printbook(bookstore):
+
+    print("BOOKSTORE\n\n")
+
+    print("'"+bookstore["New Arrivals"]["WEB"][0]+"','", bookstore["New Arrivals"]["WEB"][1]+"','",bookstore["New Arrivals"]["WEB"][2]+"','", bookstore["New Arrivals"]["WEB"][3]+"'",sep="")
+
+    print("'"+bookstore["New Arrivals"]["COOKING"][0]+"','", bookstore["New Arrivals"]["COOKING"][1]+"','",bookstore["New Arrivals"]["COOKING"][2]+"','", bookstore["New Arrivals"]["COOKING"][3]+"'",sep="")
+
+    print("'"+bookstore["New Arrivals"]["CHILDREN"][0]+"','", bookstore["New Arrivals"]["CHILDREN"][1]+"','",bookstore["New Arrivals"]["CHILDREN"][2]+"','", bookstore["New Arrivals"]["CHILDREN"][3]+"'",sep="")
+
+    
+
+
+bookstore = {
+
+    "New Arrivals":{
+
+        "COOKING":["Everyday Italian","Giada De Laurentiis","2005","30.00"],
+
+        "CHILDREN":["Harry Potter", "J K. Rowling","2005","29.99"],
+
+        "WEB":["Learning XML","Erik T. Ray","2003","39.95"]
+
+    }
+
+}
+
+
+printbook(bookstore)
+
+    
+
+
+
+######7
+str1="""Python is a widely used high-level programming language for general-purpose programming, created 
+by Guido van Rossum and first released in 1991. An interpreted language, Python has a design philosophy 
+which emphasizes code readability (notably using whitespace indentation to delimit code blocks rather than
+ curly braces or keywords), and a syntax which allows programmers to express concepts in fewer lines of code 
+than possible in languages such as C++ or Java. The language provides constructs intended to enable writing 
+clear programs on both a small and large scale. Python features a dynamic type system and automatic memory 
+management and supports multiple programming paradigms, including object-oriented, imperative, functional 
+programming, and procedural styles. It has a large and comprehensive standard library. Python interpreters 
+are available for many operating systems, allowing Python code to run on a wide variety of systems. CPython, 
+the reference implementation of Python, is open source software and has a community-based development model, 
+as do nearly all of its variant implementations. CPython is managed by the non-profit Python Software Foundation."""
+
+
+str1 = str1.replace(".","")
+
+str1 = str1.replace(",","")
+
+str1 = str1.replace(")","")
+
+str1 = str1.replace("("," ")
+
+lstr1 = str1.split(" ")
+
+result = {}
+
+
+for i in lstr1:
+
+    count = 0
+
+    if i not in result:
+
+        for j in lstr1:
+
+            if j == i:
+
+                count += 1
+
+        result[i] = count
+
+temp = []
+
+for key, value in sorted(result.items(), key=lambda item: item[1]):
+
+    temp.append(key)
+
+
+
+print(temp[-1], temp[-2],temp[-3],temp[-4],temp[-5], sep="\n")  
+
+
+#####8
+
+str1="""Python is a widely used high-level programming language for general-purpose programming, created 
+by Guido van Rossum and first released in 1991. An interpreted language, Python has a design philosophy 
+which emphasizes code readability (notably using whitespace indentation to delimit code blocks rather than
+ curly braces or keywords), and a syntax which allows programmers to express concepts in fewer lines of code 
+than possible in languages such as C++ or Java. The language provides constructs intended to enable writing 
+clear programs on both a small and large scale. Python features a dynamic type system and automatic memory 
+management and supports multiple programming paradigms, including object-oriented, imperative, functional 
+programming, and procedural styles. It has a large and comprehensive standard library. Python interpreters 
+are available for many operating systems, allowing Python code to run on a wide variety of systems. CPython, 
+the reference implementation of Python, is open source software and has a community-based development model, 
+as do nearly all of its variant implementations. CPython is managed by the non-profit Python Software Foundation."""
+
+
+
+
+str1 = str1.replace(".","")
+
+str1 = str1.replace(",","")
+
+str1 = str1.replace(")","")
+
+str1 = str1.replace("("," ")
+
+lstr1 = str1.split(" ")
+
+result = {}
+
+
+for i in lstr1:
+
+    if i not in result:
+
+        result[i] = []
+
+    for j in range(len(lstr1)-1):
+
+        if lstr1[j] == i:
+
+            if lstr1[j+1] not in result[i]:
+
+                result[i].append(lstr1[j+1])
+
+print(result)             
+         
